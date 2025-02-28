eae734b64282ca045067ac19e6ca8753400d0008
Juan Esteban Cardona <127203236+Cardona-JE@users.noreply.github.com>
2025-02-28 00:17:47 +0000

    Agrego los ejercicios de la clase 2

---
+++ Clase-2/Condicionales.java
@@ -0,0 +1,13 @@
+public class Condicionales {
+
+    public static void main(String[] args) {
+        int numero = 0;
+
+        if (numero > 0){
+            System.out.println("El numero es positivo");
+        } else if (numero < 0) {
+            System.err.println("El numero es negativo");
+        }
+    }
+    
+}
---
+++ Clase-2/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+
+    public static void main (String[] args){
+        int edad = 25;
+        double altura = 1.83;
+        String nombre = "Juan";
+        boolean isEstudiante = true;
+        System.out.println("Nombre: " + nombre + ", Edad: " + edad + ", Altura: "+ altura + ", Es un estudiante: " + isEstudiante);
+    }
+
+}
