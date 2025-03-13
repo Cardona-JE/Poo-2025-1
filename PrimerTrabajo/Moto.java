122d2aa692edad891316dc96b07a94ed679b67fa
Juan Esteban Cardona <127203236+Cardona-JE@users.noreply.github.com>
2025-03-13 18:04:09 +0000

    Creamos la carpeta del trabajo y creamos la primera clase Moto que funciona como base del programa

---
+++ PrimerTrabajo/Moto.java
@@ -0,0 +1,22 @@
+public class Moto {
+    private String placa;
+    private boolean esAltoCilindraje;
+    
+    public Moto(String placa, boolean esAltoCilindraje) {
+        this.placa = placa;
+        this.esAltoCilindraje = esAltoCilindraje;
+    }
+    
+    public String getPlaca() {
+        return placa;
+    }
+    
+    public boolean esAltoCilindraje() {
+        return esAltoCilindraje;
+    }
+    
+    @Override
+    public String toString() {
+        return "Placa: " + placa + (esAltoCilindraje ? " (Alto Cilindraje)" : " (Bajo Cilindraje)");
+    }
+}
