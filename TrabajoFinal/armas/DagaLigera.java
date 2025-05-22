756d2ce6b43c1aa5157153dcce535c87aba20fac
Juan Esteban Cardona <127203236+Cardona-JE@users.noreply.github.com>
2025-05-22 17:38:49 +0000

    Creacion de las dos armas para los personajes

---
+++ TrabajoFinal/armas/DagaLigera.java
@@ -0,0 +1,8 @@
+package armas;
+
+public class DagaLigera extends Arma {
+
+    public DagaLigera() {
+        super("Daga Ligera", 20, 0.6);
+    }
+}
---
+++ TrabajoFinal/armas/EspadaPesada.java
@@ -0,0 +1,8 @@
+package armas;
+
+public class EspadaPesada extends Arma {
+
+    public EspadaPesada() {
+        super("Espada Pesada", 25, 0.2);
+    }
+}
